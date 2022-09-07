93b0bb1605446d601bd639a87dbf6fa1b542c94f
stonislaff <yanoter1232003@gmail.com>
2022-09-07 20:55:41 +0300

    -localisation to English added

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,20 +2,42 @@ import './header.scss'
 import './mobile-header.scss'
 import {NavLink} from "react-router-dom";
 import ua_lang from "../../templates/lang/ua.svg"
+import en_lang from "../../templates/lang/en.svg"
 import burger from "../../templates/burger.svg"
 import {useEffect, useState} from "react";
+import strings from "../../localisation/localisation"
+
 function Header(){
     const [width, setWidth] = useState(window.innerWidth)
     const [height, setHeight] = useState(window.innerHeight)
     const [burgerState, setBurger] = useState(false)
     const [burgerClass, setClass] = useState('closed')
+    const [lang, swapLanguage] = useState("en")
+    const [langImg, setLanguageImg] = useState(en_lang)
+
+    const updateLanguage = () =>{
+        let setLang = 'ua'
+        let setLangImg = ua_lang
+        if (lang === "en"){
+            setLang = 'ua'
+            setLangImg = ua_lang
+        }
+        else{
+            setLang = 'en'
+            setLangImg = en_lang
+        }
+        swapLanguage(setLang)
+        setLanguageImg(setLangImg)
+        strings.setLanguage(setLang)
+    }
+
     const updateDimensions = () => {
         setWidth(window.innerWidth)
         setHeight(window.innerHeight)
     }
     const changeBurgerState = () => {
       setBurger(!burgerState)
-        if(burgerState === true){
+        if(burgerState){
             setClass('open')
         }
         else{
@@ -33,11 +55,12 @@ function Header(){
         return (
             <header>
                 <div className={burgerClass}>
-                    <img src={burger} onClick={changeBurgerState}/>
+                    <img alt='burger' src={burger} onClick={changeBurgerState}/>
                 </div>
             </header>
         )
     }
+
     else {
         return (
             <header>
@@ -46,16 +69,16 @@ function Header(){
                         <NavLink to="/"><span className="logo1">Find</span><span className="logo2">acc</span></NavLink>
                     </nav>
                     <nav className="navButtons">
-                        <NavLink to="search"><span>Пошук</span></NavLink>
-                        <NavLink to="rate"><span>Тарифи</span></NavLink>
-                        <NavLink to="about-us"><span>Про нас</span></NavLink>
+                        <NavLink to="search"><span>{strings.search}</span></NavLink>
+                        <NavLink to="rate"><span>{strings.rate}</span></NavLink>
+                        <NavLink to="about-us"><span>{strings.about}</span></NavLink>
                     </nav>
                     <nav className="loginButtons">
-                        <NavLink to="login"><span className="login">Увійти</span></NavLink>
-                        <NavLink to="sign-up"><span className="signup">Зареєструватись</span></NavLink>
+                        <NavLink to="login"><span className="login">{strings.login}</span></NavLink>
+                        <NavLink to="sign-up"><span className="signup">{strings.signup}</span></NavLink>
                     </nav>
-                    <div className="changeLanguage">
-                        <img src={ua_lang}/>
+                    <div className="changeLanguage"  onClick={updateLanguage}>
+                        <img src={langImg}/>
                     </div>
                 </nav>
             </header>
---
+++ src/localisation/localisation.js
@@ -0,0 +1,20 @@
+import LocalizedStrings from 'react-localization'
+
+let strings = new LocalizedStrings({
+    en:{
+        search:"Search",
+        rate:"Rate",
+        about:"About us",
+        login:"Log in",
+        signup:"Sign up"
+    },
+    ua:{
+        search:"Пошук",
+        rate:"Тарифи",
+        about:"Про нас",
+        login:"Вхід",
+        signup:"Реєстрація"
+    }
+});
+
+export default strings
